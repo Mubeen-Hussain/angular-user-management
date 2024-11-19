8b4c02fdf1efaacb038b499d228b32fb06b7d8e0
Mubeen Teciz <smubeen@tecizeverything.com>
2024-11-19 11:15:14 +0500

    changes

--- src/components/auth/auth.component.ts
+++ src/components/auth/auth.component.ts
@@ -64,6 +64,7 @@ export class AuthComponent {
     // Update localStorage with the new users array
     localStorage.setItem('users', JSON.stringify(this.users));
 
+
     this.snackBar.open('User registered successfully.', 'Close', { duration: 3000 });
 
     this.registerModel = new RegisterModel();
